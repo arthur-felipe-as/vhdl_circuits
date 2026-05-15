3492499c51aabb2fe9d5d8e075e65873ef11f65f
arthur-felipe-as <arthur.f.andrade.s@gmail.com>
2026-05-15 10:53:40 -0300

    nome tava errado

--- 211/demux_211b.vhd
+++ 211/demux_211b.vhd
@@ -2,13 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 
-entity mux_211 is
+entity demux_211 is
 	port( X :in std_logic_vector(2 downto 0);
 			S1, S2 : in std_logic;
 			Y1,Y2,Y3,Y4: out std_logic_vector(2 downto 0));
 end;
 
-architecture mux of mux_211 is
+architecture demux of demux_211 is
 	signal S : std_logic_vector(0 to 1);
 	begin
 	S(0) <= S1;
@@ -27,6 +27,6 @@ architecture mux of mux_211 is
 			Y4 <= X when "11",
 			"000" when others;
 		
-end mux;
+end demux;
 	
 			
--- 211/mux_211a.vhd
+++ 211/mux_211a.vhd
@@ -2,18 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 
-entity mux_211 is
+entity demux_211 is
 	port( X :in std_logic_vector(2 downto 0);
 			S1, S2: in std_logic;
 			Y1,Y2,Y3,Y4: out std_logic_vector(2 downto 0));
 end;
 
-architecture mux of mux_211 is
+architecture demux of demux_211 is
 	begin
 	Y1 <= X when (S1 = '0' and S2 = '0') else "000";
 	Y2 <= X when (S1 = '0' and S2 = '1') else "000";
 	Y3 <= X when (S1 = '1' and S2 = '0') else "000";
 	Y4 <= X when (S1 = '1' and S2 = '1') else "000";
-end mux;
+end demux;
 	
 			
