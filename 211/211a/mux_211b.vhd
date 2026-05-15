5824cc7da596ec9b42c7d250393e4248d6b15601
arthur-felipe-as <arthur.f.andrade.s@gmail.com>
2026-05-15 10:41:25 -0300

    211: Simulação e Códigos

---
+++ 211/211a/mux_211b.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity mux_211 is
+	port( X :in std_logic_vector(2 downto 0);
+			S1, S2 : in std_logic;
+			Y1,Y2,Y3,Y4: out std_logic_vector(2 downto 0));
+end;
+
+architecture mux of mux_211 is
+	signal S : std_logic_vector(0 to 1);
+	begin
+	S(0) <= S1;
+	S(1) <= S2;
+	
+	with (S) select
+			Y1 <= X when "00",
+			"000" when others;
+	with (S) select
+			Y2 <= X when "01",
+			"000" when others;
+	with (S) select
+			Y3 <= X when "10",
+			"000" when others;
+	with (S) select		
+			Y4 <= X when "11",
+			"000" when others;
+		
+end mux;
+	
+			
