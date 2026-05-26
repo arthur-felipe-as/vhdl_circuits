f08601ab48b70d3634dca72cfd4804d79110dcca
arthur-felipe-as <arthur.f.andrade.s@gmail.com>
2026-05-26 18:03:10 -0300

    maq de cafe e ula boas

---
+++ 213/maqCafe.vhd
@@ -0,0 +1,118 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+--Displlay dos botoes, numeros
+entity display7_num is
+Port(
+    hex: in std_logic_vector(2 downto 0);
+    d7: out std_logic_vector(0 to 6) --  8 1 2 4 5 6 7
+);
+end display7_num;
+
+architecture behavior1 of display7_num is
+    signal normal_d7: std_logic_vector(0 to 6);
+begin
+    
+    process(hex)
+        begin
+            case hex is
+                when "000" => normal_d7 <= "1111111"; -- 8
+                when "001" => normal_d7 <= "0110000"; -- 1
+                when "010" => normal_d7 <= "1101101"; -- 2
+                when "011" => normal_d7 <= "1111001"; -- 3
+                when "100" => normal_d7 <= "0110011"; -- 4
+                when "101" => normal_d7 <= "1011011"; -- 5
+                when "110" => normal_d7 <= "1011111"; -- 6
+                when "111" => normal_d7 <= "1110000"; -- 7
+                 when others => normal_d7 <= "0000000"; -- Desligado
+            end case;
+    end process;
+	 d7 <= not normal_d7;
+end behavior1;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+-- Display dos cafes (letras)
+entity display7_char is
+Port(
+	hex: in std_logic_vector(2 downto 0);
+    d7: out std_logic_vector(0 to 6) --  a b c d e f g h
+);
+end display7_char;
+
+architecture behavior2 of display7_char is
+signal normal_d7: std_logic_vector(0 to 6);
+
+begin
+	process(hex)
+    	begin
+        	case hex is
+                when "001" => normal_d7 <= "1110111"; -- A
+                when "010" => normal_d7 <= "0011111"; -- B
+                when "011" => normal_d7 <= "1001110"; -- C
+                when "100" => normal_d7 <= "0111101"; -- D
+                when "101" => normal_d7 <= "1001111"; -- E
+                when "110" => normal_d7 <= "1000111"; -- F
+              	when "111" => normal_d7 <= "1011110"; -- G
+              	when "000" => normal_d7 <= "0110111"; -- H
+              	when others => normal_d7 <= "0000000"; -- Desligado
+			end case;
+	end process;
+	d7 <= not normal_d7;
+end behavior2;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+ entity maqCafe is 
+ PORT(
+	botoes: in std_logic_vector(0 to 7);
+	saida_input: out std_logic_vector(0 to 6);
+ 	saida_output: out std_logic_vector(0 to 6) 
+ );
+ end maqCafe;
+
+ architecture behavior of maqCafe is
+ component display7_num is
+Port(
+    hex: in std_logic_vector(2 downto 0);
+    d7: out std_logic_vector(0 to 6) --  8 1 2 4 5 6 7
+);
+end component;
+
+component display7_char is
+Port(
+	hex: in std_logic_vector(2 downto 0);
+    d7: out std_logic_vector(0 to 6) --  a b c d e f g h
+);
+end component;
+signal botoes_bin: std_logic_vector(2 downto 0);
+signal enable_display: std_logic;	
+	begin
+	process(botoes)
+	begin
+	enable_display <= '1';
+		case botoes is
+		when "00000001" => botoes_bin <= "001";
+		when "00000010" => botoes_bin <= "010";
+		when "00000100" => botoes_bin <= "011";
+		when "00001000" => botoes_bin <= "100";
+		when "00010000" => botoes_bin <= "101";
+		when "00100000" => botoes_bin <= "110";
+		when "01000000" => botoes_bin <= "111";
+		when "10000000" => botoes_bin <= "000";
+		when others => 
+						botoes_bin <= "000";
+						enable_display <= '0';
+		end case;
+		end process;
+	mostrar_entrada : display7_num port map (botoes_bin, saida_input);
+	mostrar_saida : display7_char port map (botoes_bin, saida_output);
+	
+	
+	saida_input  <= "1111110" when enable_display = '0' else saida_input;
+   saida_output <= "1111110" when enable_display = '0' else saida_output;
+ end behavior;
--- 243/ALU_board.vhd
+++ 243/ALU_board.vhd
@@ -1,3 +1,43 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity display7 is
+Port(
+	hex: in std_logic_vector(3 downto 0);
+    d7: out std_logic_vector(0 to 6) 
+);
+end display7;
+
+architecture behavior of display7 is
+signal normal_d7: std_logic_vector(0 to 6);
+begin
+	
+	process(hex)
+    	begin
+        	case hex is
+            	when "0000" => normal_d7 <= "1111110"; -- 0
+				when "0001" => normal_d7 <= "0110000"; -- 1
+                when "0010" => normal_d7 <= "1101101"; -- 2
+                when "0011" => normal_d7 <= "1111001"; -- 3
+                when "0100" => normal_d7 <= "0110011"; -- 4
+                when "0101" => normal_d7 <= "1011011"; -- 5
+                when "0110" => normal_d7 <= "1011111"; -- 6
+                when "0111" => normal_d7 <= "1110000"; -- 7
+                when "1000" => normal_d7 <= "1111111"; -- 8
+                when "1001" => normal_d7 <= "1111011"; -- 9
+                
+                when "1010" => normal_d7 <= "1110111"; -- A
+                when "1011" => normal_d7 <= "0011111"; -- B
+                when "1100" => normal_d7 <= "1001110"; -- C
+                when "1101" => normal_d7 <= "0111101"; -- D
+                when "1110" => normal_d7 <= "1001111"; -- E
+                when "1111" => normal_d7 <= "1000111"; -- F
+			end case;
+	end process;
+	d7 <= not normal_d7; --acender = nivel logico baixo
+end behavior;
+
+
 -- Designing the Logic Extender
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -129,11 +169,21 @@ entity ALU is
 port(
 	C1, C0: in std_logic;
 	Input: in std_logic_vector(15 downto 0);
-    Output: out std_logic_vector(15 downto 0);
-    Ovf: out std_logic);
+    Output0: out std_logic_vector(0 to 6);
+    Output1: out std_logic_vector(0 to 6);
+	 Output2: out std_logic_vector(0 to 6);
+	 Output3: out std_logic_vector(0 to 6);
+	 Ovf: out std_logic);
 end ALU;
 
 architecture arch of ALU is
+	component display7 is
+	Port(
+		hex: in std_logic_vector(3 downto 0);
+		d7: out std_logic_vector(0 to 6)
+	);
+	end component;
+	
     component LogComp is
     port(
         M, S1, S0: in std_logic;
@@ -155,6 +205,7 @@ architecture arch of ALU is
     
     signal IA, IB: std_logic_vector(15 downto 0);
     signal Cin: std_logic;
+	 signal display_data: std_logic_vector(15 downto 0);
 begin
 	S_in <= Input(2 downto 0) when (C1 = '0' and C0 = '0') else S_in;
 	A_in <= Input when (C1 = '0' and C0 = '1') else A_in;
@@ -162,8 +213,29 @@ begin
 
 	LOG: LogComp port map(S_in(2), S_in(1), S_in(0), A_in, B_in, IA, IB, Cin);
     ART: Adder16Bit port map(IA, IB, Cin, F_out, Ovf);
-    
-    Output <= A_in when (C1 = '0' and C0 = '1') else
-              B_in when (C1 = '1' and C0 = '0') else
-              F_out when (C1 = '1' and C0 = '1');
+    process(C1,C0,A_in, B_in, F_out)
+	 begin
+			if (C1 = '0' and C0 = '1') then
+            display_data <= A_in;
+
+        elsif (C1 = '1' and C0 = '0') then
+            display_data <= B_in;
+
+        elsif (C1 = '1' and C0 = '1') then
+            display_data <= F_out;
+
+        else
+            display_data <= (others => '0');
+
+        end if;
+	end process;
+	 led0 : display7 port map(display_data(3 downto 0), Output0);
+
+    led1 : display7 port map(display_data(7 downto 4), Output1);
+
+    led2 : display7 port map(display_data(11 downto 8), Output2);
+		  
+    led3 : display7 port map(display_data(15 downto 12), Output3);
+
+	 
 end arch;
