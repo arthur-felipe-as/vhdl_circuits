09b9e89e35ee307151069c51afe6f827699bf29e
GuiChavesUFRN <guilherme.chaves.139@ufrn.edu.br>
2026-05-26 18:51:18 -0300

    Update maqCafe.vhd

--- 213/maqCafe.vhd
+++ 213/maqCafe.vhd
@@ -25,10 +25,10 @@ begin
                 when "101" => normal_d7 <= "1011011"; -- 5
                 when "110" => normal_d7 <= "1011111"; -- 6
                 when "111" => normal_d7 <= "1110000"; -- 7
-                 when others => normal_d7 <= "0000000"; -- Desligado
+                when others => normal_d7 <= "0000000"; -- Desligado
             end case;
     end process;
-	 d7 <= not normal_d7;
+	d7 <= not normal_d7;
 end behavior1;
 
 library IEEE;
@@ -67,35 +67,36 @@ end behavior2;
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
- entity maqCafe is 
- PORT(
-	botoes: in std_logic_vector(0 to 7);
-	saida_input: out std_logic_vector(0 to 6);
- 	saida_output: out std_logic_vector(0 to 6) 
- );
- end maqCafe;
+entity maqCafe is 
+PORT(
+    botoes: in std_logic_vector(0 to 7);
+    saida_input: out std_logic_vector(0 to 6);
+    saida_output: out std_logic_vector(0 to 6));
+end maqCafe;
 
- architecture behavior of maqCafe is
- component display7_num is
-Port(
-    hex: in std_logic_vector(2 downto 0);
-    d7: out std_logic_vector(0 to 6) --  8 1 2 4 5 6 7
-);
-end component;
+architecture behavior of maqCafe is
+	component display7_num is
+	Port(
+    	hex: in std_logic_vector(2 downto 0);
+    	d7: out std_logic_vector(0 to 6)); --  8 1 2 4 5 6 7
+	end component;
 
-component display7_char is
-Port(
-	hex: in std_logic_vector(2 downto 0);
-    d7: out std_logic_vector(0 to 6) --  a b c d e f g h
-);
-end component;
-signal botoes_bin: std_logic_vector(2 downto 0);
-signal enable_display: std_logic;	
+	component display7_char is
+	Port(
+		hex: in std_logic_vector(2 downto 0);
+    	d7: out std_logic_vector(0 to 6)); --  a b c d e f g h
+	end component;
+
+	signal disp_in, disp_out: std_logic_vector(0 to 6);
+
+	signal botoes_bin: std_logic_vector(2 downto 0);
+	signal enable_display: std_logic;
+    
 	begin
 	process(botoes)
 	begin
 	enable_display <= '1';
-		case botoes is
+	case botoes is
 		when "00000001" => botoes_bin <= "001";
 		when "00000010" => botoes_bin <= "010";
 		when "00000100" => botoes_bin <= "011";
@@ -108,11 +109,10 @@ signal enable_display: std_logic;
 						botoes_bin <= "000";
 						enable_display <= '0';
 		end case;
-		end process;
-	mostrar_entrada : display7_num port map (botoes_bin, saida_input);
-	mostrar_saida : display7_char port map (botoes_bin, saida_output);
+	end process;
+	mostrar_entrada : display7_num port map (botoes_bin, disp_in);
+	mostrar_saida : display7_char port map (botoes_bin, disp_out);
 	
-	
-	saida_input  <= "1111110" when enable_display = '0' else saida_input;
-   saida_output <= "1111110" when enable_display = '0' else saida_output;
+	saida_input  <= "1111110" when enable_display = '0' else disp_in;
+    saida_output <= "1111110" when enable_display = '0' else disp_out;
  end behavior;
